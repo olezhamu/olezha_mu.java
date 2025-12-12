20a0fe2fc31450a9da40a2bf8e9f79b7ac684585
Oleg <mushkevich.oleg@gmail.com>
2025-12-12 11:52:05 +0300

    new

---
+++ lab3/src/Action/IsUnloading.java
@@ -0,0 +1,16 @@
+package Action;
+
+import Boat.*;
+import Human.*;
+
+public class IsUnloading extends Action{
+
+    public IsUnloading(Boat Boat) {
+        super(Boat);
+    }
+
+    @Override
+    public String getResult() {
+        return "";
+    }
+}
