902d5a22468c18588463b6cd30a10a0ce325c549
Oleg <mushkevich.oleg@gmail.com>
2025-12-12 12:33:29 +0300

    new

--- lab3/src/Main.java
+++ lab3/src/Main.java
@@ -9,13 +9,6 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args){
-        /*
-        System.out.println(Hammer.getMany());
-        System.out.println(BagOfNails.getName());
-        System.out.println(I.getItems());
-        System.out.println(new IsHolding(I,new Item[] {Hammer, BagOfNails, Lom}).Hold());
-        System.out.println(new IsHoldingSoftly(I,new Item[] {Hammer, BagOfNails, Lom} ).Hold());
-         */
         Item Hammer = new Item("молоток", "небольшой", "1", "маленький", "дерево");
         Item BagOfNails = new Bag("гвозди", "небольшой", "2 или 3", "маленький", "железный");
         Item Lom = new Item("лом", "средний", "3", "средний", "железный");
@@ -26,9 +19,7 @@ public class Main {
         if (OldRaft.getAgility() < Raft.getAgility()) {
             System.out.println("плот более поворотливый, чем предыдущий");
         } else {System.out.println("плот менее поворотливый, чем предыдущий");}
-        List<Item> existingList = Arrays.asList(Hammer, BagOfNails, Lom);
-        ArrayList<Item> ToHold = new ArrayList<>(existingList);
-        ArrayList<Item> Hold = new IsHolding(I, ToHold).Hold();
+        new IsHolding(I, new ArrayList<Item>(Arrays.asList(Hammer, BagOfNails, Lom))).Hold();
         System.out.println(I.WhatsOwn());
     }
 }
