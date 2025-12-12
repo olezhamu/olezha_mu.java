f49d419724acfce55a7e4acdabd82b3cb78f15ed
Oleg <mushkevich.oleg@gmail.com>
2025-12-12 11:54:52 +0300

    new

--- lab3/src/Action/IsUnloading.java
+++ lab3/src/Action/IsUnloading.java
@@ -2,15 +2,26 @@ package Action;
 
 import Boat.*;
 import Human.*;
+import Item.Item;
+
+import java.util.ArrayList;
 
 public class IsUnloading extends Action{
+    private Boat Boat;
 
     public IsUnloading(Boat Boat) {
         super(Boat);
+        this.Boat = Boat;
+    }
+
+    public String Unloading() {
+        super.setWhatActionsAreDone(Boat.getName() + " разгружается");
+        Boat.setCargo(new ArrayList<Item>(0));
+        return Boat.getName() + " разжгужается";
     }
 
     @Override
     public String getResult() {
-        return "";
+        return Boat.getName() + " разжгужается";
     }
 }
