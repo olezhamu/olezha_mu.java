a6b5bf7877797d241cdd19476a4117468b339ec7
Oleg <mushkevich.oleg@gmail.com>
2025-12-12 12:19:40 +0300

    new

--- lab3/src/Action/Set.java
+++ lab3/src/Action/Set.java
@@ -4,12 +4,14 @@ import Boat.*;
 import Human.*;
 import Item.*;
 
+import java.util.ArrayList;
+
 public class Set {
     public Human Human;
-    public Item[] Items;
+    public ArrayList<Item> Items;
     public Boat Boat;
 
-    public Set(Human Human, Item[] Items, Boat Boat) {
+    public Set(Human Human, ArrayList<Item> Items, Boat Boat) {
         this.Human = Human;
         this.Items = Items;
         this.Boat = Boat;
