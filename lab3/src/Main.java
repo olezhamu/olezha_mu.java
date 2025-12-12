985c7a645630e1e1620f66efc74910197abc1df7
Oleg <mushkevich.oleg@gmail.com>
2025-12-12 16:56:23 +0300

    new

--- lab3/src/Main.java
+++ lab3/src/Main.java
@@ -12,14 +12,20 @@ public class Main {
         Item Hammer = new Item("молоток", "небольшой", "1", "маленький", "дерево");
         Item BagOfNails = new Bag("гвозди", "небольшой", "2 или 3", "маленький", "железный");
         Item Lom = new Item("лом", "средний", "3", "средний", "железный");
+
         Human I = new Human("Я", new ArrayList<Item>());
-        System.out.println(new IsClimbing(I, "веревка", "корабль").Climb());
+
         Boat Raft = new IsBuildingBoat(I, "плот", 10, 10).BuildBoat();
         Boat OldRaft = new Boat("старый плот", 9, 9);
+
+        Set Set = new Set(I, new ArrayList<Item>(Arrays.asList(Hammer, BagOfNails, Lom)), Raft);
+
+        System.out.println(new IsClimbing(I, "веревка", "корабль").Climb());
+
         if (OldRaft.getAgility() < Raft.getAgility()) {
             System.out.println("плот более поворотливый, чем предыдущий");
         } else {System.out.println("плот менее поворотливый, чем предыдущий");}
-        Set Set = new Set(I, new ArrayList<Item>(Arrays.asList(Hammer, BagOfNails, Lom)), Raft);
+
         Set.Hold();
         Set.Load();
         Set.Transport();
